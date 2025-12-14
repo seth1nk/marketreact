2f26e6e591ac2700dafcf6eeedca406c5e601576
seth1nk <kostik.vs10@mail.ru>
2025-12-14 20:58:07 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Удалите или закомментируйте SpeedInsights, если он вызывает ошибки при сборке,
+// но судя по вашему package.json, он установлен.
+// import { SpeedInsights } from "@vercel/speed-insights/react"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
